8d176013e0e1c7f694213111ec5779ee7fe34902
Hugh McKenzie <hugh@developingfoundation.org.au>
2024-10-05 19:48:47 +1000

    Add send to OpenAI and stream response in editor

--- main.ts
+++ main.ts
@@ -1,134 +1,133 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Plugin, PluginSettingTab, Setting, Editor } from 'obsidian';
+import OpenAI from 'openai';
 
 interface MyPluginSettings {
-	mySetting: string;
+  apiKey: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+  apiKey: '',
+};
+
+export default class MyOpenAIPlugin extends Plugin {
+  settings: MyPluginSettings;
+
+  async onload() {
+    await this.loadSettings();
+
+    // Add a command to trigger the API interaction
+    this.addCommand({
+      id: 'interact-with-openai',
+      name: 'Interact with OpenAI',
+      callback: () => this.interactWithOpenAI(),
+    });
+
+    // Add a command to send selected text to OpenAI and append the response
+    this.addCommand({
+      id: 'send-selection-to-openai',
+      name: 'Send Selection to OpenAI',
+      editorCallback: (editor: Editor) => this.handleSelectedText(editor),
+    });
+
+    // Add a settings tab for API key configuration
+    this.addSettingTab(new OpenAISettingTab(this.app, this));
+  }
+
+  async interactWithOpenAI() {
+    const client = new OpenAI({
+      apiKey: this.settings.apiKey,
+      dangerouslyAllowBrowser: true,
+    });
+
+    try {
+      const chatCompletion = await client.chat.completions.create({
+        messages: [{ role: 'user', content: 'Say this is a test' }],
+        model: 'gpt-3.5-turbo',
+      });
+
+      // Here we just log the response, but you could display it in Obsidian
+      console.log('OpenAI response:', chatCompletion.choices[0].message.content);
+    } catch (error) {
+      console.error('Error interacting with OpenAI:', error);
+    }
+  }
+
+  async handleSelectedText(editor: Editor) {
+    const client = new OpenAI({
+      apiKey: this.settings.apiKey,
+      dangerouslyAllowBrowser: true,
+    });
+
+    const selectedText = editor.getSelection();
+    if (!selectedText) {
+      console.log('No text selected.');
+      return;
+    }
+
+    const cursorPosition = editor.getCursor('to');
+
+    try {
+      const responseStream = await client.chat.completions.create({
+        messages: [{ role: 'user', content: selectedText }],
+        model: 'gpt-3.5-turbo',
+        stream: true,
+      });
+
+      let responseText = '';
+      // Add a newline before the response to separate it from the selected text
+      editor.replaceRange('\n\n', cursorPosition);
+      cursorPosition.line += 2;
+      cursorPosition.ch = 0;
+
+      for await (const part of responseStream) {
+        const content = part.choices[0]?.delta?.content;
+        if (content) {
+          responseText += content;
+          // Append new content without replacing the selection or duplicating
+          editor.replaceRange(content, cursorPosition);
+          cursorPosition.ch += content.length;
+        }
+      }
+    } catch (error) {
+      console.error('Error interacting with OpenAI:', error);
+    }
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class OpenAISettingTab extends PluginSettingTab {
+  plugin: MyOpenAIPlugin;
 
-	async onload() {
-		await this.loadSettings();
+  constructor(app: App, plugin: MyOpenAIPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+  display(): void {
+    const { containerEl } = this;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    containerEl.empty();
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    containerEl.createEl('h2', { text: 'Settings for OpenAI Plugin' });
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('OpenAI API Key')
+      .setDesc('Enter your OpenAI API key')
+      .addText((text) =>
+        text
+          .setPlaceholder('Enter your API key here...')
+          .setValue(this.plugin.settings.apiKey)
+          .onChange(async (value) => {
+            this.plugin.settings.apiKey = value;
+            await this.plugin.saveSettings();
+          })
+      );
+  }
 }
